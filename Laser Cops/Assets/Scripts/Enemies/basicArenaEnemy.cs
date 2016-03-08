a9959df41aa68429cb4fceb68f3fd23aeb12f123
Squirrell <ksquires11@gmail.com>
2016-03-08 16:47:50 -0600

    initial commit of setting up enemies and glow effect

---
+++ Laser Cops/Assets/Scripts/Enemies/basicArenaEnemy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class basicArenaEnemy : MonoBehaviour {
+    public float speed = 0.5f;
+    private Transform playerToTrack;
+    private GameObject[] players;
+	// Use this for initialization
+	void Start () {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        int randInt = Random.Range(0,players.Length);
+        playerToTrack = players[randInt].transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Laser Cops/Assets/Scripts/Enemies/basicScrollingEnemyScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class basicScrollingEnemyScript : MonoBehaviour {
+    public float speed = 0.25f;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().gravityScale = speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Tether"))
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
