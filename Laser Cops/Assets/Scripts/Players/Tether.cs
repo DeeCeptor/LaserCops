72bb5c9753b9d604df3d31c74da1ada912ad1fb1
DeeCeptor <sirmichaellong@gmail.com>
2016-03-11 18:00:09 -0600

    Added camera filters

--- Laser Cops/Assets/Scripts/Players/Tether.cs
+++ Laser Cops/Assets/Scripts/Players/Tether.cs
@@ -24,6 +24,28 @@ public class Tether : MonoBehaviour
 
     public List<GameObject> tether_links;
 
+
+    // TETHER GRAPHICS
+    public int zigs = 140;      // How many particles
+    public float speed = 1f;    // How quickly do they oscillate
+    public float scale = 1.5f;
+
+    Color primary_colour;
+    Color secondary_colour;
+
+    public Color primary_destroy_colour = Color.red;
+    public Color secondary_destroy_colour = Color.white;
+    public Color primary_capture_colour = Color.blue;
+    public Color secondary_capture_colour = Color.white;
+
+    Perlin noise;
+    float oneOverZigs;
+
+    private Particle[] particles;
+    List<GameObject> links;
+
+
+
     void Awake ()
     {
         tether = this;
@@ -37,6 +59,12 @@ public class Tether : MonoBehaviour
         line.SetWidth(left_width, right_width);
 
         SetTetherMode(TetherMode.Destroy);
+
+        oneOverZigs = 1f / (float)zigs;
+        GetComponent<ParticleEmitter>().emit = false;
+
+        GetComponent<ParticleEmitter>().Emit(zigs);
+        particles = GetComponent<ParticleEmitter>().particles;
     }
 
 
@@ -56,21 +84,80 @@ public class Tether : MonoBehaviour
     {
         if (mode == TetherMode.Destroy)
         {
-
+            primary_colour = primary_destroy_colour;
+            secondary_colour = secondary_destroy_colour;
         }
         else if (mode == TetherMode.Capture)
         {
-
+            primary_colour = primary_capture_colour;
+            secondary_colour = secondary_capture_colour;
         }
         Debug.Log("Setting tether " + mode);
         cur_tether_mode = mode;
     }
 
 
-	void Update ()
+    public void SetTetherLayer()
+    {
+
+    }
+
+
+    void Update()
     {
         cur_tether_switching_cooldown -= Time.deltaTime;
-	    // Pulsates between the 2 colours, end from end
+        // Pulsates between the 2 colours, end from end
         //line.SetColors()
-	}
+
+        UpdateTetherGraphics();
+    }
+
+
+    public void UpdateTetherGraphics()
+    {
+        if (links == null)
+            links = Tether.tether.tether_links;
+
+        if (noise == null)
+            noise = new Perlin();
+
+        float timex = Time.time * speed * 0.1365143f;
+        float timey = Time.time * speed * 1.21688f;
+        //float timez = Time.time * speed * 2.5564f;
+
+        for (int i = 0; i < particles.Length; i++)
+        {
+            int cur_link = (int)(((float)links.Count) * ((float)i / (float)particles.Length));
+            int prev_link = Mathf.Max(0, cur_link - 1);//(int)(((float)links.Count) * ((float)Mathf.Max(0, (i - 1)) / (float)particles.Length));
+            float t = ((float)i % (float)links.Count / (float)links.Count);
+
+            //Vector3 position = links[prev_link].transform.position * t + links[cur_link].transform.position * (1 - t);
+            Vector2 position = Vector2.Lerp(links[prev_link].transform.position,
+                links[cur_link].transform.position,
+                t);
+            //Debug.Log(i + " : " + position.x + " : between : " + links[prev_link].transform.position + links[cur_link].transform.position + t + " : prev: " + prev_link + " cur: " + cur_link);
+            /*
+            Vector3 position = Vector3.Lerp(
+                links[(int)(((float)links.Count) * ((float)Mathf.Max(0, (i - 1))/ (float)particles.Length))].transform.position, 
+                links[(int) (((float) links.Count) * ((float) i / (float) particles.Length))].transform.position, 
+                oneOverZigs * (float)i);*/
+            //Vector3 position = Vector3.Lerp(transform.position, target.position, oneOverZigs * (float)i);
+            Vector2 offset = new Vector3(noise.Noise(timex + position.x, timex + position.y, 0),
+                                        noise.Noise(timey + position.x, timey + position.y, 0),
+                                        0);
+            //position += (offset * scale * ((float)i * oneOverZigs));
+            position += (offset * scale * ((float)20 * oneOverZigs));
+
+            particles[i].velocity = (Vector2)particles[i].velocity;
+            particles[i].position = position;
+            particles[i].energy = 99999f;
+
+            // Oscillate colour
+            // Red / white is cool staticy look
+            particles[i].color = Color.Lerp(primary_colour, secondary_colour, Random.value);
+            //particles[i].color = Color.white;
+        }
+
+        GetComponent<ParticleEmitter>().particles = particles;
+    }
 }
