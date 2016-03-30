f51aecb79c1a7e3653df8722c753d90a2f8b8cee
Squirrell <ksquires11@gmail.com>
2016-03-30 15:38:09 -0600

    imported assets added a couple of scripts and worked on warning markers for fast things

---
+++ Laser Cops/Assets/DieOverTime.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//destroy object after a certain number of seconds
+public class DieOverTime : MonoBehaviour {
+
+    public float secondsTillDeath = 2f;
+    private float deathCounter = 0f;
+	// Use this for initialization
+	void Start () {
+        deathCounter = secondsTillDeath + Time.time;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	    if(deathCounter < Time.time)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
