b8bf4952fa5320644c2bad8852544aeaa5352e77
DeeCeptor <sirmichaellong@gmail.com>
2016-04-27 14:02:21 -0600

    no message

--- Laser Cops/Assets/Scripts/PositionBoundaryOffscreen.cs
+++ Laser Cops/Assets/Scripts/PositionBoundaryOffscreen.cs
@@ -4,29 +4,74 @@ using System.Collections;
 public class PositionBoundaryOffscreen : MonoBehaviour
 {
     BoxCollider2D box;
-    public bool left_side_of_screen = true;
+    GameObject highway;
+    public enum Match_Screen_Or_Highway { Screen, Highway };
+    public Match_Screen_Or_Highway Matching;
+    public enum Side_Of_Screen { Left, Right, Top, Bottom };
+    public Side_Of_Screen Side;
 
     void Start ()
     {
         box = this.GetComponent<BoxCollider2D>();
-        AdjustPosition();
+        highway = GameObject.FindGameObjectWithTag("Grid");
+
+        switch (Matching)
+        {
+            case Match_Screen_Or_Highway.Screen:
+                AdjustPositionByScreen();
+                break;
+            case Match_Screen_Or_Highway.Highway:
+                AdjustPositionByHighway();
+                break;
+        }
     }
 
 
-    public void AdjustPosition()
+    public void AdjustPositionByScreen()
     {
         Vector3 minScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
         Vector3 maxScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
 
-        Vector2 new_position;
-        if (left_side_of_screen)
+        Vector2 new_position = Vector2.zero;
+        switch (Side)
         {
-            new_position = new Vector2(minScreenBounds.x - box.bounds.extents.x, 0);
+            case Side_Of_Screen.Left:
+                new_position = new Vector2(minScreenBounds.x - box.bounds.extents.x, 0);
+                break;
+            case Side_Of_Screen.Right:
+                new_position = new Vector2(maxScreenBounds.x + box.bounds.extents.x, 0);
+                break;
+            case Side_Of_Screen.Bottom:
+                new_position = new Vector2(0, minScreenBounds.y - box.bounds.extents.y);
+                break;
+            case Side_Of_Screen.Top:
+                new_position = new Vector2(0, maxScreenBounds.y + box.bounds.extents.y);
+                break;
         }
-        else
+
+        this.transform.position = new_position;
+    }
+    public void AdjustPositionByHighway()
+    {
+        MeshRenderer mesh = highway.GetComponent<MeshRenderer>();
+        Vector3 minScreenBounds = mesh.bounds.min;
+        Vector3 maxScreenBounds = mesh.bounds.max;
+
+        Vector2 new_position = Vector2.zero;
+        switch (Side)
         {
-            // Right side
-            new_position = new Vector2(maxScreenBounds.x + box.bounds.extents.x, 0);
+            case Side_Of_Screen.Left:
+                new_position = new Vector2(minScreenBounds.x - box.bounds.extents.x, 0);
+                break;
+            case Side_Of_Screen.Right:
+                new_position = new Vector2(maxScreenBounds.x + box.bounds.extents.x, 0);
+                break;
+            case Side_Of_Screen.Bottom:
+                new_position = new Vector2(0, minScreenBounds.y - box.bounds.extents.y);
+                break;
+            case Side_Of_Screen.Top:
+                new_position = new Vector2(0, maxScreenBounds.y + box.bounds.extents.y);
+                break;
         }
 
         this.transform.position = new_position;
